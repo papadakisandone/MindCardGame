acb2d1dd56a02969bb96799daa465c360f4c7dc8
Antonios Papadakis <83516016+papadakisandone@users.noreply.github.com>
2021-05-03 18:17:12 +0200

    add comment

--- guesscard.py
+++ guesscard.py
@@ -1,6 +1,7 @@
 import random
 import time
 #import numpy as np
+#Antonis Papadakis
 
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 shades = ["♠", "♥", "♦", "♣"]
