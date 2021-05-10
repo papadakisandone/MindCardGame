7652306366fd53c128d3a9acb1bc8daada915492
Antonios Papadakis <83516016+papadakisandone@users.noreply.github.com>
2021-05-10 19:46:38 +0200

    fix deck bug with cards

--- guesscard.py
+++ guesscard.py
@@ -3,7 +3,7 @@ import time
 #import numpy as np
 #Antonis Papadakis
 
-numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 shades = ["♠", "♥", "♦", "♣"]
 
 deck = []  # create deck
